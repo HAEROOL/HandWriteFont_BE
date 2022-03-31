d03e13e5bf013f85d2576c814e123fe822a520ae
Woo-Yeol <king199276@gmail.com>
2022-03-31 15:10:04 +0900

    우열::폰트 DB 제작 과정에 대한 상태 추가

--- handwritefont_be/main/models.py
+++ handwritefont_be/main/models.py
@@ -4,12 +4,16 @@ from user.models import HWFUser
 def user_directory_path(instance, filename):
     return 'media/font/{0}/{1}'.format(instance.owner.nickname, filename)
 
+def status_field():
+    return [('Accept','Accept'),('To Do','To Do'),('Doing','Doing'),('Done','Done'),('Canceled','Canceled')]
+
 class Font(models.Model):
     name = models.CharField(primary_key=True, max_length=20)
     created_date = models.DateField(auto_now_add=True)
     like_users = models.ManyToManyField(HWFUser, blank=True, related_name='like')
     owner = models.ForeignKey(HWFUser, related_name='font', on_delete=models.CASCADE)
-    file = models.FileField(upload_to=user_directory_path)
+    file = models.FileField(upload_to=user_directory_path,null=True,blank=True)
+    status = models.CharField(max_length=10, choices=status_field(), default='Canceled')
 
     @property
     def like_num(self):
